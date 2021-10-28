33351e87871bf65a8320cff4e1b5fd993adbe923
Nouman Shahid <nshahid@sandsmedia.com>
2021-10-28 10:15:10 +0200

    vue added

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 console.log('hello world');
 console.log(data.Articles);
+
+
+var articles = data.Articles;
+var articlesVue = new Vue({
+        el: "#articlesVue",
+        data: {
+            articles: articles,
+            message: 'Hello Vue.js!'
+        }
+
+    });
