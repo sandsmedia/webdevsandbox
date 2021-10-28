95066d85ba4a31dc79f999b8753ffc76f888233f
Nouman Shahid <nshahid@sandsmedia.com>
2021-10-28 09:46:34 +0200

    skeleton for js and css with articles done

---
+++ articles.js
@@ -0,0 +1,275 @@
+var data ={
+  "status":"ok",
+  "Articles": [
+          {
+            "_id": "e4bd3087fa5ebf43856ca2b4",
+            "name": "Das erwarten unsere Experten von PHP 8",
+            "type": "INTERVIEW",
+            "subtitle": "Aussicht auf die achte Runde PHP",
+            "abstract": "PHP 8 steht kurz vor dem finalen Release. Ein guter Zeitpunkt, um einige Stimmen aus der Community zu sammeln: Auf welche Features freuen sich Entwickler am meisten? Welche fehlen? Und was kann von PHP 8 generell erwartet werden?",
+            "publishedAt": "29/10/2020 08:37:42",
+            "teaserImage": {
+              "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/articles/e4bd3087fa5ebf43856ca2b4/images/teaserImage_xx_expert check php_1606405536657.jpg"
+            },
+            "authors": [
+              {
+                "_id": "09eb5f4f63c8dc67719c4858",
+                "name": "Jérémy Pastouret",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/09eb5f4f63c8dc67719c4858/images/avatarSquareSmall_xx_pastouret_jeremy_ek_1604065038623.jpg"
+                }
+              },
+              {
+                "_id": "53e5f3815dd46df12006b2ba",
+                "name": "Susanne Moog",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/53e5f3815dd46df12006b2ba/images/avatarSquareSmall_xx_moog_susanne_ek_1604064991419.jpg"
+                }
+              },
+              {
+                "_id": "7ad600f0067562190ce27984",
+                "name": "Jörg Moldenhauer",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/7ad600f0067562190ce27984/images/avatarSquareSmall_xx_moldenhauer_joerg_ek_1600952869128.jpg"
+                }
+              },
+              {
+                "_id": "f1784d052a80114d78709e3e",
+                "name": "Brent Roose",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/f1784d052a80114d78709e3e/images/avatarSquareSmall_xx_roose_brent_ek_1604065063241.jpg"
+                }
+              },
+              {
+                "_id": "f74bc4f97c6b8b2aa9da24f7",
+                "name": "Vitalij Mik",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/f74bc4f97c6b8b2aa9da24f7/images/avatarSquareSmall_xx_mik_vitalij_ek_1594024635202.jpg"
+                }
+              }
+            ]
+          },
+          {
+            "_id": "f7db2a0e38b0199e1fcc1cfe",
+            "name": "„PHP 8.0 ist eine evolutionäre Weiterentwicklung“",
+            "type": "INTERVIEW",
+            "subtitle": "Interview mit Florian Sowade",
+            "abstract": "PHP Magazin: PHP 8 kommt bald. Wie schätzt du die neue Version ein? Ist es ein großer Meilenstein für die PHP-Community oder eine eher unspektakuläre, evolutionäre Weiterentwicklung?",
+            "publishedAt": "26/08/2020 12:32:13",
+            "teaserImage": {
+              "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/articles/f7db2a0e38b0199e1fcc1cfe/images/teaserImage_xx_Florian Sowade 2_1606406615802.jpg"
+            },
+            "authors": [
+              {
+                "_id": "4b307d343b6aa576dd8905c8",
+                "name": "Florian Sowade",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/4b307d343b6aa576dd8905c8/images/avatarSquareSmall_xx_sowade_florian_ek_1598516074527.jpg"
+                }
+              }
+            ]
+          },
+          {
+            "_id": "403524577c7dec9468035870",
+            "name": "25 Jahre PHP",
+            "type": "INTERVIEW",
+            "subtitle": "Fünf PHP-Experten im Interview",
+            "abstract": "PHP wurde 1995 veröffentlicht und feiert 2020 ein Vierteljahrhundert. Anlässlich dessen haben wir fünf PHP-Experten und Sprecher der International PHP Conference befragt, welche Rolle PHP für sie heute noch spielt und was sie sich für PHP 8.0 wünschen. Glaubt man zahlreichen Statistiken, ist PHP auf dem absteigenden Ast - doch unsere Experten sind hier anderer Meinung.",
+            "publishedAt": "14/11/2019 15:11:41",
+            "teaserImage": {
+              "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/articles/403524577c7dec9468035870/images/teaserImage_xx_18_Bergmann_rodnikovay_Naty_Lee_1576746183871.jpg"
+            },
+            "authors": [
+              {
+                "_id": "3c61165258e2c06c01da39e0",
+                "name": "Ralf Eggert",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/3c61165258e2c06c01da39e0/images/avatarSquareSmall_xx_eggert_ralf_ek_1554901115508.jpg"
+                }
+              },
+              {
+                "_id": "8e991fcef5098e1bc8a0ceb7",
+                "name": "Sebastian Bergmann",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/8e991fcef5098e1bc8a0ceb7/images/avatarSquareSmall_xx_bergmann_sebastian_ek.jpg"
+                }
+              },
+              {
+                "_id": "be9d1a6bd5c90d2b34cf0bdb",
+                "name": "Sebastian Feldmann",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/be9d1a6bd5c90d2b34cf0bdb/images/avatarSquareSmall_xx_feldmann_sebastian_ek_1576833116661.jpg"
+                }
+              },
+              {
+                "_id": "c8f10c9fb1941a2cb2204507",
+                "name": "Arne Blankerts",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/c8f10c9fb1941a2cb2204507/images/avatarSquareSmall_xx_blankerts_arne_ek.jpg"
+                }
+              },
+              {
+                "_id": "e2159d1ba586fde3101117e5",
+                "name": "Marcel Normann",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/e2159d1ba586fde3101117e5/images/avatarSquareSmall_xx_normann_marcel_ek_1576833171611.jpg"
+                }
+              }
+            ]
+          },
+          {
+            "_id": "a8d2e7e15017b8437123fe70",
+            "name": "Neuer PHP-Verein verfolgt große Ziele",
+            "type": "INTERVIEW",
+            "subtitle": "Interview mit Stefan Priebsch",
+            "abstract": "PHP Magazin: Wer sind die Initiatoren und an wen richtet sich der Verein?",
+            "publishedAt": "18/12/2018 00:00:00",
+            "teaserImage": {
+              "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/articles/a8d2e7e15017b8437123fe70/images/teaserImage_xx_EKiosk_Teaser_Rubrik_PM_Magazin_1597327063714.jpg"
+            },
+            "authors": [
+              {
+                "_id": "efd03d49435ad1420927324d",
+                "name": "Stefan Priebsch",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/efd03d49435ad1420927324d/images/avatarSquareSmall_xx_priebsch_stefan_ek_1557743153834.jpg"
+                }
+              }
+            ]
+          },
+          {
+            "_id": "c2aecaeb1d4658cb8ecd01b4",
+            "name": "Quo vadis PHP?",
+            "type": "INTERVIEW",
+            "subtitle": "Interview mit Arne Blankerts",
+            "abstract": "PHP Magazin: Einen Blogbeitrag hast du vor Kurzem mit „Why PHP 7.2 Is Important“ betitelt. Kannst du bitte noch einmal zusammenfassen, was das Release an interessanten Neuerungen gebracht hat?",
+            "publishedAt": "23/04/2018 00:00:00",
+            "teaserImage": {
+              "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/articles/c2aecaeb1d4658cb8ecd01b4/images/teaserImage_xx_EKiosk_Teaser_Rubrik_PM_Magazin_1597327063714.jpg"
+            },
+            "authors": [
+              {
+                "_id": "c8f10c9fb1941a2cb2204507",
+                "name": "Arne Blankerts",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/c8f10c9fb1941a2cb2204507/images/avatarSquareSmall_xx_blankerts_arne_ek.jpg"
+                }
+              }
+            ]
+          },
+          {
+            "_id": "c7098f033174b026475ced19",
+            "name": "TYPO3 und Platform.sh - ein Traumpaar",
+            "type": "INTERVIEW",
+            "subtitle": "Interview mit Benni Mack",
+            "abstract": "PHP Magazin: Herr Mack, seit letztem Jahr arbeitet TYPO3 mit Platform.sh als Cloud-Hostingdienst zusammen. Warum haben Sie sich gerade für diesen Anbieter entschieden?",
+            "publishedAt": "26/04/2017 00:00:00",
+            "teaserImage": {
+              "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/articles/c7098f033174b026475ced19/images/teaserImage_xx_Brand_Teaser_PHPMagazin_shutterstock_saicle_1558424827817.jpg"
+            },
+            "authors": [
+              {
+                "_id": "391d3c2add3311d6689367ee",
+                "name": "Benni Mack",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/391d3c2add3311d6689367ee/images/avatarSquareSmall_xx_mack_benni_ek_1557237577881.jpg"
+                }
+              }
+            ]
+          },
+          {
+            "_id": "df16d085d5a9d1ba51df306f",
+            "name": "„PHPUnit 6 setzt lange gehegte Wünsche um.“",
+            "type": "INTERVIEW",
+            "subtitle": "PHPUnit 6 kam mit vielen Neuerungen - und einigen Problemen",
+            "abstract": "PHPUnit 6.0.0 wurde veröffentlicht. Es ist die erste Version des Testing-Frameworks, das voll und ganz auf PHP 7.x setzt und den Support für PHP 5.x über Bord fallen lässt. Doch das Release hatte so seine Tücken …",
+            "publishedAt": "24/02/2017 00:00:00",
+            "teaserImage": {
+              "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/articles/df16d085d5a9d1ba51df306f/images/teaserImage_xx_32_Wiesseckel_1561453178512.jpg"
+            },
+            "authors": [
+              {
+                "_id": "8e991fcef5098e1bc8a0ceb7",
+                "name": "Sebastian Bergmann",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/8e991fcef5098e1bc8a0ceb7/images/avatarSquareSmall_xx_bergmann_sebastian_ek.jpg"
+                }
+              }
+            ]
+          },
+          {
+            "_id": "6d6694eb34d6d2b23b9365e9",
+            "name": "Die Welle",
+            "type": "INTERVIEW",
+            "subtitle": "Zend wurde von Rogue Wave übernommen",
+            "abstract": "Zend wurde vom US-Cross-Plattform-Spezialisten Rogue Wave aufgekauft. Ein Deal, der Zends Vorstoß in den Enterprise-Bereich noch weiter beschleunigen könnte.",
+            "publishedAt": "01/11/2015 00:00:00",
+            "teaserImage": {
+              "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/articles/6d6694eb34d6d2b23b9365e9/images/teaserImage_xx_Brand_Teaser_PHPMagazin_shutterstock_saicle_1558424827817.jpg"
+            },
+            "authors": [
+              {
+                "_id": "3c61165258e2c06c01da39e0",
+                "name": "Ralf Eggert",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/3c61165258e2c06c01da39e0/images/avatarSquareSmall_xx_eggert_ralf_ek_1554901115508.jpg"
+                }
+              }
+            ]
+          },
+          {
+            "_id": "f3d92ffd9062ffe4695df994",
+            "name": "„Ich wünsche mir, dass die PHP-Welt offen für diese Technologie ist.“",
+            "type": "INTERVIEW",
+            "subtitle": "In Java sind Application Server gang und gäbe - warum nicht davon lernen?",
+            "abstract": "Mit dem appserver.io Iron Horse wurde Version 1.0 der ambitionierten Infrastrukturlösung veröffentlicht. Der appserver.io soll klassische Enterprise-Ansätze in die PHP-Welt transferieren und Entwicklern dabei ein ganzes Set von Services bieten, mit denen sich die Arbeit in großen und performanceintensiven Applikationen erleichtern lässt. Das Beste daran: Es bietet Multi-Threading von Haus aus, ist komplett Open Source und hat keine Probleme mit Systemen, die eigentlich gar nicht dafür entwickelt wurden. Wir haben die Gunst der Stunde genutzt und mit einem der Köpfe hinter dem Projekt gesprochen.",
+            "publishedAt": "02/03/2015 00:00:00",
+            "teaserImage": {
+              "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/articles/f3d92ffd9062ffe4695df994/images/teaserImage_xx_Brand_Teaser_PHPMagazin_shutterstock_saicle_1558424827817.jpg"
+            },
+            "authors": [
+              {
+                "_id": "680a53965a3c2e4888f1aefe",
+                "name": "Stefan Willkommer",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/680a53965a3c2e4888f1aefe/images/avatarSquareSmall_xx_willkommer_stefan_ek_1557911554972.jpg"
+                }
+              }
+            ]
+          },
+          {
+            "_id": "cb814df9cc5cd26aafbd7d62",
+            "name": "„Die Teams sind auf  einem guten Weg.“",
+            "type": "INTERVIEW",
+            "subtitle": "Interview mit Sebastian Bergmann, Stefan Priebsch und Arne Blankerts",
+            "abstract": "Auf der IPC im Frühling letzten Jahres haben wir uns mit Stefan Priebsch und Sebastian Bergmann unterhalten. Seitdem ist fast ein Jahr vergangen, ein neues Major Release hat das Licht der Welt erblickt. Grund genug zu fragen, was sich im letzten Jahr getan hat.",
+            "publishedAt": "27/04/2012 00:00:00",
+            "teaserImage": {
+              "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/articles/cb814df9cc5cd26aafbd7d62/images/teaserImage_xx_Brand_Teaser_PHPMagazin_shutterstock_saicle_1558424827817.jpg"
+            },
+            "authors": [
+              {
+                "_id": "8e991fcef5098e1bc8a0ceb7",
+                "name": "Sebastian Bergmann",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/8e991fcef5098e1bc8a0ceb7/images/avatarSquareSmall_xx_bergmann_sebastian_ek.jpg"
+                }
+              },
+              {
+                "_id": "c8f10c9fb1941a2cb2204507",
+                "name": "Arne Blankerts",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/c8f10c9fb1941a2cb2204507/images/avatarSquareSmall_xx_blankerts_arne_ek.jpg"
+                }
+              },
+              {
+                "_id": "efd03d49435ad1420927324d",
+                "name": "Stefan Priebsch",
+                "avatarSquareSmall": {
+                  "xx": "https://s3.eu-west-1.amazonaws.com/redsys-stage/authors/efd03d49435ad1420927324d/images/avatarSquareSmall_xx_priebsch_stefan_ek_1557743153834.jpg"
+                }
+              }
+            ]
+          }
+        ]
+}
